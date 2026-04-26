bff9cd23629e7609166eb32b1b17eef3eca551c8
nange <cctonange@gmail.com>
2026-04-26 13:16:40 +0800

    simply code

--- app/src/main/java/com/musan/easysstun/EasyssTunApplication.kt
+++ app/src/main/java/com/musan/easysstun/EasyssTunApplication.kt
@@ -1,40 +1,10 @@
 package com.musan.easysstun
 
 import android.app.Application
-import android.content.Context
-import android.content.Intent
-import android.net.Uri
-import android.os.Build
-import android.os.PowerManager
-import android.provider.Settings
-import android.util.Log
 
 class EasyssTunApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
-
-        // 请求忽略电池优化
-        requestIgnoreBatteryOptimizations()
-    }
-
-    private fun requestIgnoreBatteryOptimizations() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            val powerManager = getSystemService(Context.POWER_SERVICE) as PowerManager
-            val packageName = packageName
-
-            if (!powerManager.isIgnoringBatteryOptimizations(packageName)) {
-                Log.i("EasyssTunApp", "Requesting battery optimization whitelist")
-                try {
-                    val intent = Intent(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS).apply {
-                        data = Uri.parse("package:$packageName")
-                        flags = Intent.FLAG_ACTIVITY_NEW_TASK
-                    }
-                    startActivity(intent)
-                } catch (e: Exception) {
-                    Log.e("EasyssTunApp", "Failed to request battery optimization: ${e.message}")
-                }
-            }
-        }
     }
 }
